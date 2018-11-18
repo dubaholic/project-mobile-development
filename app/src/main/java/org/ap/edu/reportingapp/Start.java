90d522a84356bdc68d50a1deb13126cccd4ee82c
MaartenExelmans <s090083@ap.be>
2018-11-19 00:25:14 +0100

    Added seekerBar
    - Added working seekerBar
    - Added "opmerkingen"
    - Added "btnVerzenden"
    - Updated layout

--- app/src/main/java/org/ap/edu/reportingapp/Start.java
+++ app/src/main/java/org/ap/edu/reportingapp/Start.java
@@ -2,6 +2,8 @@ package org.ap.edu.reportingapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -12,16 +14,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.IOException;
 
 public class Start extends AppCompatActivity {
-    private String[] categorie, verdiepingen, lokaalMin1, lokaalGelijkVloers, lokaal1ste, lokaal2de, lokaal3de, lokaal4de, lokaalDak, lokalen, leeg = {""};
-    private String cmbVerdiepingValue;
+    //moet dit private zijn?
+    private String[] verdiepingen, lokaalMin1, lokaalGelijkVloers, lokaal1ste, lokaal2de, lokaal3de,
+            lokaal4de, lokaalDak, leeg = {""},  lokalen, categorie, urgenties;
+    private String verdiepingValue, urgentieColorString = "#FFA500";
+    private int seekBarValue = 2;
+
     private Spinner cmbLokaal, cmbVerdieping, cmbCategorie;
     private ArrayAdapter<String> adapterLokaal, adapterVerdieping, adapterCategorie;
-    private Button btnFoto;
+    private Button btnFoto, btnVerzenden;
+    private SeekBar sldUrgentie;
+    private TextView txtUrgentieValue;
+
     private Uri filePath;
 
     private final int PICK_IMAGE_REQUEST = 71;
@@ -40,11 +51,15 @@ public class Start extends AppCompatActivity {
         lokaal4de = getResources().getStringArray(R.array.lokaal4de);
         lokaalDak = getResources().getStringArray(R.array.lokaalDak);
         categorie = getResources().getStringArray(R.array.categorie);
+        urgenties = getResources().getStringArray(R.array.urgenties);
 
         cmbVerdieping = findViewById(R.id.cmbVerdieping);
         cmbLokaal = findViewById(R.id.cmbLokaal);
         cmbCategorie = findViewById(R.id.cmbCategorie);
         btnFoto = findViewById(R.id.btnFoto);
+        btnVerzenden = findViewById(R.id.btnVerzenden);
+        sldUrgentie = findViewById(R.id.sldUrgentie);
+        txtUrgentieValue = findViewById(R.id.txtViewUrgentieValue);
 
         adapterVerdieping = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, verdiepingen);
         adapterCategorie = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categorie);
@@ -52,32 +67,36 @@ public class Start extends AppCompatActivity {
         cmbVerdieping.setAdapter(adapterVerdieping);
         cmbCategorie.setAdapter(adapterCategorie);
 
+        //Default values voor seekerbar
+        sldUrgentie.getProgressDrawable().setColorFilter(Color.parseColor(urgentieColorString), PorterDuff.Mode.MULTIPLY);
+        txtUrgentieValue.setText(urgenties[seekBarValue]);
+
         cmbVerdieping.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View selectedItemView, int position, long id) {
-                cmbVerdiepingValue = cmbVerdieping.getSelectedItem().toString();
+                verdiepingValue = cmbVerdieping.getSelectedItem().toString();
                 cmbLokaal.setEnabled(true);
 
                 //veranderen in switch?
-                if (cmbVerdiepingValue.equals("-1")){
+                if (verdiepingValue.equals("-1")){
                     lokalen = lokaalMin1;
                 }
-                else if (cmbVerdiepingValue.equals("Gelijkvloers")){
+                else if (verdiepingValue.equals("Gelijkvloers")){
                     lokalen = lokaalGelijkVloers;
                 }
-                else if (cmbVerdiepingValue.equals("1ste")){
+                else if (verdiepingValue.equals("1ste")){
                     lokalen = lokaal1ste;
                 }
-                else if (cmbVerdiepingValue.equals("2de")){
+                else if (verdiepingValue.equals("2de")){
                     lokalen = lokaal2de;
                 }
-                else if (cmbVerdiepingValue.equals("3de")){
+                else if (verdiepingValue.equals("3de")){
                     lokalen = lokaal3de;
                 }
-                else if (cmbVerdiepingValue.equals("4de")){
+                else if (verdiepingValue.equals("4de")){
                     lokalen = lokaal4de;
                 }
-                else if (cmbVerdiepingValue.equals("Dak")){
+                else if (verdiepingValue.equals("Dak")){
                     lokalen = lokaalDak;
                 }
                 else{
@@ -103,6 +122,41 @@ public class Start extends AppCompatActivity {
             }
         });
 
+        sldUrgentie.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarValue = progress;
+                txtUrgentieValue.setText(urgenties[seekBarValue]);
+                if (seekBarValue == 0){
+                    urgentieColorString = "#00FF00";
+                }
+                else if (seekBarValue == 1){
+                    urgentieColorString = "#FFFF00";
+                }
+                else if (seekBarValue == 2){
+                    urgentieColorString = "#FFA500";
+                }
+                else if (seekBarValue == 3){
+                    urgentieColorString = "#FF0000";
+                }
+                else {
+                    urgentieColorString = "#8B0000";
+                }
+                sldUrgentie.getProgressDrawable().setColorFilter(Color.parseColor(urgentieColorString), PorterDuff.Mode.MULTIPLY);
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        btnVerzenden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Item is verzonden
+            }
+        });
     }
     private void chooseImage() {
         Intent intent = new Intent();
