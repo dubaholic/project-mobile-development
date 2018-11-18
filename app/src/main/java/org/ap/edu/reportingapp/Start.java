48d334665823acfb6e6e786ab6b3f2ea2932bf2a
MaartenExelmans <s090083@ap.be>
2018-11-18 19:18:55 +0100

    Fixed project, Added spinners
    - Fixed project not starting
    - Fixed start screen layout
    - Added dynamic spinners
    - Added spinner values

--- app/src/main/java/org/ap/edu/reportingapp/Start.java
+++ app/src/main/java/org/ap/edu/reportingapp/Start.java
@@ -8,33 +8,74 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class Start extends AppCompatActivity {
-
-    Spinner cmbLokaal = findViewById(R.id.cmbLokaal);
-
-    String[] verdiepingen = new String[]{"-1", "gelijkvloers", "1ste", "2de", "3de", "4de", "dak"};
-
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, verdiepingen);
-
+    String[] verdiepingen, lokaalMin1, lokaalGelijkVloers, lokaal1ste, lokaal2de, lokaal3de, lokaal4de, lokaalDak, lokalen, leeg = {""};
+    String cmbVerdiepingValue;
+    Spinner cmbLokaal, cmbVerdieping;
+    ArrayAdapter<String> adapterLokaal, adapterVerdieping;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        cmbLokaal.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        verdiepingen = getResources().getStringArray(R.array.verdiepingen);
+        adapterVerdieping = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, verdiepingen);
+
+        lokaalMin1 = getResources().getStringArray(R.array.lokaalMin1);
+        lokaalGelijkVloers = getResources().getStringArray(R.array.lokaalGelijkVloers);
+        lokaal1ste = getResources().getStringArray(R.array.lokaal1ste);
+        lokaal2de = getResources().getStringArray(R.array.lokaal2de);
+        lokaal3de = getResources().getStringArray(R.array.lokaal3de);
+        lokaal4de = getResources().getStringArray(R.array.lokaal4de);
+        lokaalDak = getResources().getStringArray(R.array.lokaalDak);
+
+        cmbVerdieping = findViewById(R.id.cmbVerdieping);
+        cmbLokaal = findViewById(R.id.cmbLokaal);
+
+        cmbVerdieping.setAdapter(adapterVerdieping);
+
+        cmbVerdieping.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String lokaalIndex = (String) parentView.getSelectedItem();
-                switch (lokaalIndex){
-                    case "-1":
+            public void onItemSelected(AdapterView<?> parent, View selectedItemView, int position, long id) {
+                cmbVerdiepingValue = cmbVerdieping.getSelectedItem().toString();
+                cmbLokaal.setEnabled(true);
+
+                //veranderen in switch?
+                if (cmbVerdiepingValue.equals("-1")){
+                    lokalen = lokaalMin1;
                 }
+                else if (cmbVerdiepingValue.equals("Gelijkvloers")){
+                    lokalen = lokaalGelijkVloers;
+                }
+                else if (cmbVerdiepingValue.equals("1ste")){
+                    lokalen = lokaal1ste;
+                }
+                else if (cmbVerdiepingValue.equals("2de")){
+                    lokalen = lokaal2de;
+                }
+                else if (cmbVerdiepingValue.equals("3de")){
+                    lokalen = lokaal3de;
+                }
+                else if (cmbVerdiepingValue.equals("4de")){
+                    lokalen = lokaal4de;
+                }
+                else if (cmbVerdiepingValue.equals("Dak")){
+                    lokalen = lokaalDak;
+                }
+                else{
+                    lokalen = leeg;
+                    cmbLokaal.setEnabled(false);
+                }
+                adapterLokaal = new ArrayAdapter<>(Start.this, android.R.layout.simple_spinner_dropdown_item, lokalen);
+                cmbLokaal.setAdapter(adapterLokaal);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
+                //niet mogelijk denk ik? default waarde
+                //is deze code nodig?
+                cmbLokaal.setEnabled(false);
             }
-
         });
     }
 
